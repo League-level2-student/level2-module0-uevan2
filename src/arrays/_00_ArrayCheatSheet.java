48c3a5cd36db325e44349f6da9c254317442728f
League Student <league@iMac-11.attlocal.net>
2019-12-28 11:23:15 -0800

    level 2 mod 0

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,51 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+			String[] why = {"what", "is", "no", "point", "of this"};
 		//2. print the third element in the array
-		
+			System.out.println(why[2]);
 		//3. set the third element to a different value
-		
+			why[2]="the";
 		//4. print the third element again
-		
+			System.out.println(why[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i = 0; i<why.length; i++) {
+			System.out.println(why[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+			int[] fiftyNumbers= new int[50];
+			Random r = new Random();
+			int random;
 		//7. use a for loop to make every value of the integer array a random number
-		
+			for(int j = 0; j<fiftyNumbers.length; j++) {
+				random=r.nextInt();
+				fiftyNumbers[j]=random;
+			}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+			int smallest = fiftyNumbers[0];
+			for(int k = 0; k<fiftyNumbers.length; k++) {
+				if(fiftyNumbers[k]<smallest) {
+					smallest=fiftyNumbers[k];
+				}
+			}
+			System.out.println(smallest);
 		//9 print the entire array to see if step 8 was correct
-		
+			for(int l = 0; l<fiftyNumbers.length; l++) {
+				System.out.println(fiftyNumbers[l]);
+			}
 		//10. print the largest number in the array.
+			int largest = fiftyNumbers[0];
+			for(int m = 0; m<fiftyNumbers.length; m++) {
+				if(fiftyNumbers[m]>largest) {
+					largest=fiftyNumbers[m];
+				}
+			}
+			System.out.println(largest);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,10 +1,37 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+		public static void main(String[] args) {
+			Robot[] r = new Robot[5];
+			Random rr = new Random();
+			int random;
+			boolean whetherRace=true;
+			
+			for(int j =0; j<r.length; j++) {
+				r[j] = new Robot();
+				r[j].setSpeed(10);
+				r[j].setX(250+(j*100));
+				r[j].setY(500);
+			}
+			
+			while(whetherRace=true) {
+				for(int i =0; i<r.length; i++) {
+					random=rr.nextInt(50);
+					r[i].move(random);
+					if(r[i].getY()<100) {
+						whetherRace=false;
+					break;
+					}
+				}
+			}
+		}
 	//2. create an array of 5 robots.
-
+			
 	//3. use a for loop to initialize the robots.
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
